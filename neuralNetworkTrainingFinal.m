147171f8893b3b3ac1666d452e4751b1f43f1b9f
Sebastian <sebasdarias95@gmail.com>
2018-11-21 17:10:16 -0800

    Small changes

--- neuralNetworkTrainingFinal.m
+++ neuralNetworkTrainingFinal.m
@@ -1,13 +1,13 @@
+net=nunchucknet9s %Network that will be the basis of the training
 
 %Create image datastore of all fake nunchucks with the label from the foldernames
-nunAlex_ds=imageDatastore('FakeNunchuckImages','IncludeSubfolders',true,'LabelSource','foldernames');
+fakeNun_ds=imageDatastore('FakeNunchuckImages','IncludeSubfolders',true,'LabelSource','foldernames');
 
 %Split the image datastore into a three different datastores (Trainging, testing, validation)
-[testImgs,trainImgs,validationImgs] = splitEachLabel(nunAlex_ds,0.2,0.793,'Randomize');
+[testImgs,trainImgs,validationImgs] = splitEachLabel(fakeNun_ds,0.2,0.793,'Randomize');
 
-%numClasses = numel(categories(nunAlex_ds.Labels));
+%numClasses = numel(categories(fakeNun_ds.Labels));
 
-net=nunchucknet9s %Network that will be the basis of the training
 
 layers=net.Layers; %Gets the layers of the network
 
@@ -17,5 +17,5 @@ layers=net.Layers; %Gets the layers of the network
 %Training options
 options = trainingOptions('sgdm','InitialLearnRate', 0.01,'LearnRateSchedule','piecewise', 'LearnRateDropPeriod',1,'LearnRateDropFactor',0.1,'Plots','training-progress','MaxEpochs',30,'MiniBatchSize',360,'Shuffle','every-epoch','ValidationData',validationImgs,'ValidationFrequency',100,'ValidationPatience',4)
 
-%Beging neural network training
+%Begin neural network training
 [nunchucknet10s,info10s] = trainNetwork(trainImgs, layers, options);
