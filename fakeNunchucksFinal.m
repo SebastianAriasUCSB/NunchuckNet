c6b3640ebb12181766b608a6a25f2bec4b6c596c
Sebastian <sebasdarias95@gmail.com>
2018-12-01 18:39:33 -0800

    testing stuff

--- fakeNunchucksFinal.m
+++ fakeNunchucksFinal.m
@@ -145,7 +145,8 @@ for i=1:length
     image(round(point(2))-1:round(point(2))+1,round(point(1))-1:round(point(1))+1)=uint8(brightness);
     %marks a 2x2 area at next point with input brightness
 
-    if (rand<0.01) %chance that it will happen
+    if (rand<0.10) %chance that it will happen
+        disp("bias change")
         angleBias=angleBias+(1*rand-0.5) %possibility for arm curvature to change 
     end
 end
